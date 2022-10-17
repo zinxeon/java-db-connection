0fdfd14bc2f1ed30e71b32cb275e56586fafc0e3
DESKTOP-PJ36R0M\zinxeon <zinxeon@gmail.com>
2022-10-18 00:00:51 +0900

    Java DB 연동 1. INSERT 하기

---
+++ src/main/java/com/dbexercise/UserDao.java
@@ -0,0 +1,32 @@
+package com.dbexercise;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class UserDao {
+    public void add() throws SQLException, ClassNotFoundException {
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES(?, ?, ?)");
+        ps.setString(1, "1");
+        ps.setString(2, "jinseon");
+        ps.setString(3, "1123");
+
+        ps.executeUpdate();
+        ps.close();
+        conn.close();
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+        userDao.add();
+    }
+}
