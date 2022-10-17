5d6b46237a9c8f938c1a5ef04a26ac1bf826e5e9
DESKTOP-PJ36R0M\zinxeon <zinxeon@gmail.com>
2022-10-18 01:09:42 +0900

    Java DB 연동 4. SELECT 하기

--- src/main/java/com/dbexercise/UserDao.java
+++ src/main/java/com/dbexercise/UserDao.java
@@ -1,9 +1,8 @@
 package com.dbexercise;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import com.dbexercise.domain.User;
+
+import java.sql.*;
 import java.util.Map;
 
 public class UserDao {
@@ -16,10 +15,9 @@ public class UserDao {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
         PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES(?, ?, ?)");
-        ps.setString(1, "0");
-        ps.setString(2, "jixnseon");
+        ps.setString(1, "11");
+        ps.setString(2, "jinseon");
         ps.setString(3, "1123");
-
 
         int status = ps.executeUpdate();
         System.out.println(status);
@@ -29,8 +27,30 @@ public class UserDao {
         System.out.println("DB insert 성공");
     }
 
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+        PreparedStatement ps = conn.prepareStatement("SELECT id, name, password FROM users WHERE id = ?");
+        ps.setString(1, id);
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User(rs.getString("id"),
+                rs.getString("name"), rs.getString("password"));
+        rs.close();
+        ps.close();
+        conn.close();
+        return user;
+    }
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
         userDao.add();
+        User user = userDao.get("5");
+        System.out.println(user.getName());
     }
 }
